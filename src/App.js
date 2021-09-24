a954613b06839697b386673f4ed3e2d0d8b3328e
Dushyant pratap singh <74450982+dushyant2901@users.noreply.github.com>
2021-09-24 21:49:57 +0530

    updated database and links

--- src/App.js
+++ src/App.js
@@ -72,6 +72,29 @@ export default function App() {
           ))}
         </ul>
       </div>
+      <div className="links">
+        <ul className="list-unbulleted-list">
+          <li className="list-item-inline">
+            <a className="link" href="https://github.com/dushyant2901">
+              Github
+            </a>
+          </li>
+          <li className="list-item-inline">
+            <a className="link" href="https://twitter.com/DushyantPS">
+              {" "}
+              Twitter
+            </a>
+          </li>
+          <li className="list-item-inline">
+            <a
+              className="link"
+              href="https://in.linkedin.com/in/dushyant-pratap-singh-04b24619b"
+            >
+              Linkedin
+            </a>
+          </li>
+        </ul>{" "}
+      </div>
     </div>
   );
 }
